9801c1ba304b4800874681117c641d463614d698
Nick Largey <Nick.Largey@gmail.com>
2019-07-18 23:03:45 -0400

    Add files via upload

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -1,16 +1,24 @@
-import randint
+import random
 import pprint
 
+pp = pprint.PrettyPrinter(indent=4)
 
 class Board:
 
 	def __init__(self):
-		self.width = width
-		self.height = height
+		# self.width = width
+		# self.height = height
+
+		self.height = 9
+		self.width = 9
 
 		self.board = []
-		for i in range(0, height):
-			row = []
-			for j in  range(0, width):
-				row.append(blank)
-			self.board.append(row)
+		for i in range(0, self.height):
+			self.row = []
+			for j in  range(0, self.width):
+				self.row.append('')
+			self.board.append(self.row)
+
+		pp.pprint(self.board)
+
+Board()
